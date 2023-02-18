87e4e1ad35aa3c2bf525c28b876bff7bcfa8ba08
intrepido896 <104647936+intrepido896@users.noreply.github.com>
2023-02-18 12:55:09 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,51 +1,58 @@
 
 
 
+from flask import Flask, render_template, request
+import json
+import machinetranslation as mt
+
+
+app = Flask(__name__)
+
+@app.route("/englishToFrench")
+def englishToFrench():
+    textToTranslate = request.args.get('textToTranslate')
+    french_text = mt.translator.english_to_french(textToTranslate)
+    return french_text
+
+@app.route("/frenchToEnglish")
+def frenchToEnglish():
+    textToTranslate = request.args.get('textToTranslate')
+    english_text = mt.translator.french_to_english(textToTranslate)
+    return english_text
+
+@app.route("/")
+def renderIndexPage():
+    return render_template('index.html')
+
+@app.route("/about")
+def view_about_page():
+    return render_template("about.html", title="About project")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
 
-translator.py a directory
-server my package with
-__init___.py
 
 
 
-from flask import Flask, request, render_template
-
-# Declare a Flask app
-app = Flask(__name__) 
-# Main function 
-# ------------------
-	
-
-#type of function
 
 
-@app.route('/', methods=['GET', 'POST'])
-
-# provide root endpoint which renders index.html
-
-@app.route('/index.html')
 
 
-# provide root endpoint  translate from english to frech
-
-app.route('/trans1')
-
-# provide root endpoint translate from french to english
 
 
-app.route('/trans2')
 
-def main(): 
 
-# If a form is submitted
- if request.method == "POST": 
- 
- 
-  # Get values through input bars 
-  Function = request.form.get("Function")
+
+
+
+
+
+
+
   
   
   
@@ -54,71 +61,5 @@ def main():
     
 
     
-    if Function==0 :(
-	print ("English to French")
-	
-	def trans1(Function,text)
-	
-	import json 
-	from ibm_watson import LanguageTranslatorV3 
-	from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
- authenticator = IAMAuthenticator('{apikey}') 
- language_translator = LanguageTranslatorV3
- ( version='2018-05-01', authenticator=authenticator )
-  language_translator.set_service_url('{url}') 
-  translation = language_translator.translate( 
-  	 text=request.form.get("text"), 
-  	model_id='en-fr').get_result() 
-  	translate=(json.dumps(translation, indent=2, ensure_ascii=False))
-	 
-	  # Get translate
-	  
-	    return render_template("index.html", output = translate)
-	)
 
-elif Function==1: (
-	print ("French to English")
-	
-		def trans2(Function,text)
-		import json 
-	from ibm_watson import LanguageTranslatorV3 
-	from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
- authenticator = IAMAuthenticator('{apikey}') 
- language_translator = LanguageTranslatorV3
- ( version='2018-05-01', authenticator=authenticator )
-  language_translator.set_service_url('{url}') 
-  translation = language_translator.translate( 
-  	 text=request.form.get("text"), 
-  	model_id='fr-en').get_result() 
-  	translate=(json.dumps(translation, indent=2, ensure_ascii=False))
-	 
-	  # Get translate
-	  
-	    return render_template("index.html", output = translate)
-	
-	
-	
-	
-)
-
-else:(
-	
-	 print ("Enter valid Function")
-)
-
-    
-    
-    
-    
-    
-  
-
-
-
- # Running the app
-	 if __name__ == '__main__
-app.run(debug=true)
-
-
-
-
+	    
